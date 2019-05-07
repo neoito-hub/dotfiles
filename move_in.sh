d686dc064b621c437b9a8c43290143c142830cb5
Ashish Kurian Thomas <ashish.k@neoito.com>
2019-05-07 12:31:07 +0530

    script update: moving the new files into home

--- move_in.sh
+++ move_in.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 # CREDITS: https://github.com/jcs/dotfiles/blob/master/move_in.sh
 
+BACKUP_DATE=$(date +"%Y%m%d")
 
 echo "-> Backing up"
-[ -f $HOME/.zshrc ] && cp $HOME/.zshrc $HOME/.zshrc.backup
-[ -f $HOME/.vimrc ] && cp $HOME/.zshrc $HOME/.vimrc.backup
-[ -f $HOME/.tmux.conf ] && cp $HOME/.zshrc $HOME/.tmux.conf.backup
+[ -f $HOME/.zshrc ] && mv $HOME/.zshrc $HOME/.zshrc.backup.$BACKUP_DATE
+[ -f $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/.vimrc.backup.$BACKUP_DATE
+[ -f $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.backup.$BACKUP_DATE
 
 # check if we have the repo already cloned
 # do a pull if present
 # do a clone if not present
+echo "-> Clonning repo"
 if [ -d ~/.dotfiles ]; then
 	cd ~/.dotfiles
 	git pull --ff-only
@@ -17,3 +19,10 @@ if [ -d ~/.dotfiles ]; then
 else
 	git clone --recursive https://github.com/neoito-hub/dotfiles ~/.dotfiles
 fi
+
+echo "-> Installing"
+cd ~/.dotfiles
+for f in .???*; do
+	rm -f ~/$f
+	(cd ~/; ln -s .dotfiles/$f $f)
+done
