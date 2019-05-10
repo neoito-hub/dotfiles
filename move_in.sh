20b7513ec3babbebaa9eaba729e85e43845e41e0
terminalforlife <terminalforlife@yahoo.com>
2019-05-10 01:06:13 +0100

    Updated move_in.sh; made further use of bash

--- move_in.sh
+++ move_in.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 # CREDITS: https://github.com/jcs/dotfiles/blob/master/move_in.sh
 
-BACKUP_DATE=$(date +"%Y%m%d")
+printf -v BACKUP_DATE "%(%Y%m%d)T"
 CONF_FILES=".zshrc .vimrc .tmux.conf"
 
 echo "-> Backing up"
-[ -f $HOME/.zshrc ] && mv $HOME/.zshrc $HOME/.zshrc.backup.$BACKUP_DATE
-[ -f $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/.vimrc.backup.$BACKUP_DATE
-[ -f $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.backup.$BACKUP_DATE
+for FILE in $HOME/.{zshrc,vimrc,tmux.conf}; {
+	[ -f "$FILE" ] && mv "$FILE" "$HOME/${FILE##*/}.backup.$BACKUP_DATE"
+}
 
 echo "-> Making vim dirs"
-mkdir -p $HOME/.vim/files/backup
-mkdir -p $HOME/.vim/files/swap
-mkdir -p $HOME/.vim/files/undo
-mkdir -p $HOME/.vim/files/info/viminfo
+mkdir -p $HOME/.vim/files/{backup,swap,undo,info/viminfo}
 touch $HOME/.zshrc.mystuff.zsh
 
 # check if we have the repo already cloned
 # do a pull if present
 # do a clone if not present
 echo "-> Clonning repo"
-if [ -d ~/.dotfiles ]; then
-	cd ~/.dotfiles
+if cd ~/.dotfiles 2>&-; then
 	git pull --ff-only
 	git submodule update --init --recursive
 else
@@ -41,4 +37,3 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
 
 echo "-> All done."
-exit 0
